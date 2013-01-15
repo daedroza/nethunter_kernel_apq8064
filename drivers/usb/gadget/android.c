65a5502ebbf395938a1068a629b5bc2af12a92c0
Stephen Boyd <sboyd@codeaurora.org>
2013-01-14 16:41:42 -0800

    Drop whitespace, Kconfigs, Makefile noise and dead code
    Drop whitespace, unused Kconfigs, and bad Makefile entries. Undo
    code movement, remove dead function, and delete entire files that
    we don't compile that make our code divergent from upstream. No
    functional difference.
    
    Conflicts:
    	arch/arm/mach-msm/board-8064.c
    	drivers/input/touchscreen/Makefile
    	drivers/misc/Makefile
    	drivers/power/Kconfig
    	drivers/usb/gadget/ci13xxx_udc.h
    
    Change-Id: I1150b3d4b317fb5180c5d26e2366dccb28a6bf43
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -2709,6 +2709,7 @@ static ssize_t enable_store(struct device *pdev, struct device_attribute *attr,
 	int enabled = 0;
 	static DEFINE_RATELIMIT_STATE(rl, 10*HZ, 1);
 
+
 	if (!cdev)
 		return -ENODEV;
 
