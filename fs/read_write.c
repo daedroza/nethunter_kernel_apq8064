8b2d25b17bf81d1bbe19b17336ae552b6432f1b2
Kalpaj Chaudhari <daedroza@gmail.com>
2016-10-30 19:55:57 +0530

    Revert "Don't bother with redoing rw_verify_area() from default_file_splice_from()"
    This reverts commit cdd05fa57b32a40e24e9dfeeea78e57f47842b0c.

--- fs/read_write.c
+++ fs/read_write.c
@@ -16,7 +16,6 @@
 #include <linux/pagemap.h>
 #include <linux/splice.h>
 #include <linux/compat.h>
-#include "internal.h"
 
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
@@ -428,30 +427,6 @@ ssize_t do_sync_write(struct file *filp, const char __user *buf, size_t len, lof
 }
 
 EXPORT_SYMBOL(do_sync_write);
-
-ssize_t __kernel_write(struct file *file, const char *buf, size_t count, loff_t *pos)
-{
-	mm_segment_t old_fs;
-	const char __user *p;
-	ssize_t ret;
-
-	old_fs = get_fs();
-	set_fs(get_ds());
-	p = (__force const char __user *)buf;
-	if (count > MAX_RW_COUNT)
-		count =  MAX_RW_COUNT;
-	if (file->f_op->write)
-		ret = file->f_op->write(file, p, count, pos);
-	else
-		ret = do_sync_write(file, p, count, pos);
-	set_fs(old_fs);
-	if (ret > 0) {
-		fsnotify_modify(file);
-		add_wchar(current, ret);
-	}
-	inc_syscw(current);
-	return ret;
-}
 
 ssize_t vfs_write(struct file *file, const char __user *buf, size_t count, loff_t *pos)
 {
